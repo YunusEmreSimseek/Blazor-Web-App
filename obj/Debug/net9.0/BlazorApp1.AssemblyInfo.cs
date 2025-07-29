1e2fad04db97804b36e5e550ca8ece107c48a1e0
YunusEmreSimseek <yemresimseek@gmail.com>
2025-07-29 10:03:37 +0200

    localization added for exchanges page

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d07bb390b63ef5696d89501aff40bb8677c40c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9df0816ade8c2bcd6f42137c114692fa41f78dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
